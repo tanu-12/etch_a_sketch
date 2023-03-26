f23cea759ebb4e63a25e9210bed65477cf9e9035
Tanu <tanupandey291099@gmail.com>
2023-03-26 22:56:45 +0530

    created a 16 x 16 playground

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const container = document.querySelector(".container");
 const range = document.getElementById("grid-select");
-for (let i = 1; i <= 16; i++) {
+for (let i = 1; i <= 256; i++) {
     let div = document.createElement("div");
     div.className = "gridItem";
+    div.textContent = " ";
+    container.appendChild(div);
 }
